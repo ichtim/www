64c0c3909bc2c954a4e537087c58fd50f9f93fd9
ichtim <91052758+ichtim@users.noreply.github.com>
2024-10-30 00:55:13 +0100

    Add files via upload

---
+++ www/start.php
@@ -0,0 +1,18 @@
+<?php
+$include_directory="";
+?>
+<!DOCTYPE html>
+<html lang ="de">
+<head>
+<meta charset="utf-8>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="keywords" content="funfug, Startseite, start, Hauptseite">
+<title>Startseite</title>
+<link rel=stylesheet" href="css/start.css">
+</head>
+<body>
+<?php
+include("${include_directory}html/footer.html");
+?>
+</body>
+</html>
